370075c49d2d428af1674c8fc6db517c08da2e12
Vatsal Sakariya <vatsal.sakariya@toddleapp.com>
2023-03-18 11:39:38 +0530

    added animation to mobile menu

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -22,7 +22,8 @@ const MobileMenu: FunctionComponent<Props> = ({ pageLinks }) => {
 
   useEffect(() => {
     const handleClickOutside = (e: MouseEvent) => {
-      if (modalRef.current && !modalRef.current.contains(e.target as Node)) setIsOpen(false);
+      if (modalRef.current && !modalRef.current.contains(e.target as Node))
+        setIsOpen(false);
     };
 
     document.addEventListener("click", handleClickOutside);
@@ -32,7 +33,7 @@ const MobileMenu: FunctionComponent<Props> = ({ pageLinks }) => {
   return (
     <>
       {isOpen && (
-        <div className="fixed inset-0 z-40 bg-zinc-900/50 backdrop-blur focus:outline-none">
+        <div className="fixed inset-0 z-40 bg-zinc-900/50 backdrop-blur animate-in fade-in focus:outline-none">
           <div
             ref={modalRef}
             className="z-50 mx-5 mt-5 rounded-lg border border-zinc-200 bg-zinc-50 p-5 text-zinc-600 shadow-lg dark:border-zinc-600 dark:bg-zinc-700 dark:text-zinc-200"
@@ -48,7 +49,11 @@ const MobileMenu: FunctionComponent<Props> = ({ pageLinks }) => {
                 stroke="currentColor"
                 className="h-5 w-5 cursor-pointer transition-colors active:text-blue-600 dark:active:text-blue-400"
               >
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
               </svg>
             </div>
             <ul className="mt-5 divide-y divide-zinc-200 dark:divide-zinc-600">
@@ -79,7 +84,11 @@ const MobileMenu: FunctionComponent<Props> = ({ pageLinks }) => {
           stroke="currentColor"
           className="h-4 w-4"
         >
-          <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+          />
         </svg>
       </button>
     </>
